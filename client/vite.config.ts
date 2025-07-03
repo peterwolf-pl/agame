d10c9ec4bf8735dc3b3019d0ac2acd769d5a620d
peterwolf-pl <84736182+peterwolf-pl@users.noreply.github.com>
2025-07-04 01:00:52 +0200

    Fix vite config and update dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: { proxy: { '/api': 'http://localhost:3000' } },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
 })
